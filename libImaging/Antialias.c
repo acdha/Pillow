8e72b76bd6dbd9fa2c1f03f730c68a692bd794e1
Chris Adams <chris@improbable.org>
2012-03-13 11:14:01 -0700

    Consistently format source (Antialias.c)
    For sanity:
    gindent --no-tabs -i4 -br -ce -brf -cli2 -l110 --no-space-after-function-call-names

--- libImaging/Antialias.c
+++ libImaging/Antialias.c
@@ -2,7 +2,7 @@
  * The Python Imaging Library
  * $Id$
  *
- * pilopen antialiasing support 
+ * pilopen antialiasing support
  *
  * history:
  * 2002-03-09 fl  Created (for PIL 1.1.3)
@@ -20,30 +20,30 @@
 /* resampling filters (from antialias.py) */
 
 struct filter {
-    float (*filter)(float x);
+    float (*filter) (float x);
     float support;
 };
 
-static inline float sinc_filter(float x)
-{
+static inline float
+sinc_filter(float x) {
     if (x == 0.0)
         return 1.0;
     x = x * M_PI;
     return sin(x) / x;
 }
 
-static inline float antialias_filter(float x)
-{
+static inline float
+antialias_filter(float x) {
     /* lanczos (truncated sinc) */
     if (-3.0 <= x && x < 3.0)
-        return sinc_filter(x) * sinc_filter(x/3);
+        return sinc_filter(x) * sinc_filter(x / 3);
     return 0.0;
 }
 
 static struct filter ANTIALIAS = { antialias_filter, 3.0 };
 
-static inline float nearest_filter(float x)
-{
+static inline float
+nearest_filter(float x) {
     if (-0.5 <= x && x < 0.5)
         return 1.0;
     return 0.0;
@@ -51,19 +51,19 @@ static inline float nearest_filter(float x)
 
 static struct filter NEAREST = { nearest_filter, 0.5 };
 
-static inline float bilinear_filter(float x)
-{
+static inline float
+bilinear_filter(float x) {
     if (x < 0.0)
         x = -x;
     if (x < 1.0)
-        return 1.0-x;
+        return 1.0 - x;
     return 0.0;
 }
 
 static struct filter BILINEAR = { bilinear_filter, 1.0 };
 
-static inline float bicubic_filter(float x)
-{
+static inline float
+bicubic_filter(float x) {
     /* FIXME: double-check this algorithm */
     /* FIXME: for best results, "a" should be -0.5 to -1.0, but we'll
        set it to zero for now, to match the 1.1 magnifying filter */
@@ -71,9 +71,9 @@ static inline float bicubic_filter(float x)
     if (x < 0.0)
         x = -x;
     if (x < 1.0)
-        return (((a + 2.0) * x) - (a + 3.0)) * x*x + 1;
+        return (((a + 2.0) * x) - (a + 3.0)) * x * x + 1;
     if (x < 2.0)
-        return (((a * x) - 5*a) * x + 8) * x - 4*a;
+        return (((a * x) - 5 * a) * x + 8) * x - 4 * a;
     return 0.0;
 #undef a
 }
@@ -81,8 +81,7 @@ static inline float bicubic_filter(float x)
 static struct filter BICUBIC = { bicubic_filter, 2.0 };
 
 Imaging
-ImagingStretch(Imaging imOut, Imaging imIn, int filter)
-{
+ImagingStretch(Imaging imOut, Imaging imIn, int filter) {
     /* FIXME: this is a quick and straightforward translation from a
        python prototype.  might need some further C-ification... */
 
@@ -95,26 +94,24 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
 
     /* check modes */
     if (!imOut || !imIn || strcmp(imIn->mode, imOut->mode) != 0)
-	return (Imaging) ImagingError_ModeError();
+        return (Imaging) ImagingError_ModeError();
 
     /* check filter */
     switch (filter) {
-    case IMAGING_TRANSFORM_NEAREST:
-        filterp = &NEAREST;
-        break;
-    case IMAGING_TRANSFORM_ANTIALIAS:
-        filterp = &ANTIALIAS;
-        break;
-    case IMAGING_TRANSFORM_BILINEAR:
-        filterp = &BILINEAR;
-        break;
-    case IMAGING_TRANSFORM_BICUBIC:
-        filterp = &BICUBIC;
-        break;
-    default:
-        return (Imaging) ImagingError_ValueError(
-            "unsupported resampling filter"
-            );
+      case IMAGING_TRANSFORM_NEAREST:
+          filterp = &NEAREST;
+          break;
+      case IMAGING_TRANSFORM_ANTIALIAS:
+          filterp = &ANTIALIAS;
+          break;
+      case IMAGING_TRANSFORM_BILINEAR:
+          filterp = &BILINEAR;
+          break;
+      case IMAGING_TRANSFORM_BICUBIC:
+          filterp = &BICUBIC;
+          break;
+      default:
+          return (Imaging) ImagingError_ValueError("unsupported resampling filter");
     }
 
     if (imIn->ysize == imOut->ysize) {
@@ -124,7 +121,7 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
         /* prepare for vertical stretch */
         filterscale = scale = (float) imIn->ysize / imOut->ysize;
     } else
-	return (Imaging) ImagingError_Mismatch();
+        return (Imaging) ImagingError_Mismatch();
 
     /* determine support size (length of resampling filter) */
     support = filterp->support;
@@ -133,7 +130,7 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
         filterscale = 1.0;
         support = 0.5;
     }
-    
+
     support = support * filterscale;
 
     /* coefficient buffer (with rounding safety margin) */
@@ -154,7 +151,7 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
                 ymin = 0.0;
             ymax = ceil(center + support);
             if (ymax > (float) imIn->ysize)
-		ymax = (float) imIn->ysize;
+                ymax = (float) imIn->ysize;
             for (y = (int) ymin; y < (int) ymax; y++) {
                 float w = filterp->filter((y - center + 0.5) * ss) * ss;
                 k[y - (int) ymin] = w;
@@ -179,44 +176,44 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
                         imOut->image8[yy][xx] = (UINT8) ss;
                 }
             } else
-                switch(imIn->type) {
-                case IMAGING_TYPE_UINT8:
-                    /* n-bit grayscale */
-                    for (xx = 0; xx < imOut->xsize*4; xx++) {
-                        /* FIXME: skip over unused pixels */
-                        ss = 0.0;
-                        for (y = (int) ymin; y < (int) ymax; y++)
-                            ss = ss + (UINT8) imIn->image[y][xx] * k[y-(int) ymin];
-                        ss = ss * ww + 0.5;
-                        if (ss < 0.5)
-                            imOut->image[yy][xx] = (UINT8) 0;
-                        else if (ss >= 255.0)
-                            imOut->image[yy][xx] = (UINT8) 255;
-                        else
-                            imOut->image[yy][xx] = (UINT8) ss;
-                    }
-                    break;
-                case IMAGING_TYPE_INT32:
-                    /* 32-bit integer */
-                    for (xx = 0; xx < imOut->xsize; xx++) {
-                        ss = 0.0;
-                        for (y = (int) ymin; y < (int) ymax; y++)
-                            ss = ss + IMAGING_PIXEL_I(imIn, xx, y) * k[y - (int) ymin];
-                        IMAGING_PIXEL_I(imOut, xx, yy) = (int) ss * ww;
-                    }
-                    break;
-                case IMAGING_TYPE_FLOAT32:
-                    /* 32-bit float */
-                    for (xx = 0; xx < imOut->xsize; xx++) {
-                        ss = 0.0;
-                        for (y = (int) ymin; y < (int) ymax; y++)
-                            ss = ss + IMAGING_PIXEL_F(imIn, xx, y) * k[y - (int) ymin];
-                        IMAGING_PIXEL_F(imOut, xx, yy) = ss * ww;
-                    }
-                    break;
-                default:
-                    ImagingSectionLeave(&cookie);
-                    return (Imaging) ImagingError_ModeError();
+                switch (imIn->type) {
+                  case IMAGING_TYPE_UINT8:
+                      /* n-bit grayscale */
+                      for (xx = 0; xx < imOut->xsize * 4; xx++) {
+                          /* FIXME: skip over unused pixels */
+                          ss = 0.0;
+                          for (y = (int) ymin; y < (int) ymax; y++)
+                              ss = ss + (UINT8) imIn->image[y][xx] * k[y - (int) ymin];
+                          ss = ss * ww + 0.5;
+                          if (ss < 0.5)
+                              imOut->image[yy][xx] = (UINT8) 0;
+                          else if (ss >= 255.0)
+                              imOut->image[yy][xx] = (UINT8) 255;
+                          else
+                              imOut->image[yy][xx] = (UINT8) ss;
+                      }
+                      break;
+                  case IMAGING_TYPE_INT32:
+                      /* 32-bit integer */
+                      for (xx = 0; xx < imOut->xsize; xx++) {
+                          ss = 0.0;
+                          for (y = (int) ymin; y < (int) ymax; y++)
+                              ss = ss + IMAGING_PIXEL_I(imIn, xx, y) * k[y - (int) ymin];
+                          IMAGING_PIXEL_I(imOut, xx, yy) = (int) ss *ww;
+                      }
+                      break;
+                  case IMAGING_TYPE_FLOAT32:
+                      /* 32-bit float */
+                      for (xx = 0; xx < imOut->xsize; xx++) {
+                          ss = 0.0;
+                          for (y = (int) ymin; y < (int) ymax; y++)
+                              ss = ss + IMAGING_PIXEL_F(imIn, xx, y) * k[y - (int) ymin];
+                          IMAGING_PIXEL_F(imOut, xx, yy) = ss * ww;
+                      }
+                      break;
+                  default:
+                      ImagingSectionLeave(&cookie);
+                      return (Imaging) ImagingError_ModeError();
                 }
         }
     } else {
@@ -230,7 +227,7 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
                 xmin = 0.0;
             xmax = ceil(center + support);
             if (xmax > (float) imIn->xsize)
-		xmax = (float) imIn->xsize;
+                xmax = (float) imIn->xsize;
             for (x = (int) xmin; x < (int) xmax; x++) {
                 float w = filterp->filter((x - center + 0.5) * ss) * ss;
                 k[x - (int) xmin] = w;
@@ -255,47 +252,47 @@ ImagingStretch(Imaging imOut, Imaging imIn, int filter)
                         imOut->image8[yy][xx] = (UINT8) ss;
                 }
             } else
-                switch(imIn->type) {
-                case IMAGING_TYPE_UINT8:
-                    /* n-bit grayscale */
-                    for (yy = 0; yy < imOut->ysize; yy++) {
-                        for (b = 0; b < imIn->bands; b++) {
-                            if (imIn->bands == 2 && b)
-                                b = 3; /* hack to deal with LA images */
-                            ss = 0.0;
-                            for (x = (int) xmin; x < (int) xmax; x++)
-                                ss = ss + (UINT8) imIn->image[yy][x*4+b] * k[x - (int) xmin];
-                            ss = ss * ww + 0.5;
-                            if (ss < 0.5)
-                                imOut->image[yy][xx*4+b] = (UINT8) 0;
-                            else if (ss >= 255.0)
-                                imOut->image[yy][xx*4+b] = (UINT8) 255;
-                            else
-                                imOut->image[yy][xx*4+b] = (UINT8) ss;
-                        }
-                    }
-                    break;
-                case IMAGING_TYPE_INT32:
-                    /* 32-bit integer */
-                    for (yy = 0; yy < imOut->ysize; yy++) {
-                        ss = 0.0;
-                        for (x = (int) xmin; x < (int) xmax; x++)
-                            ss = ss + IMAGING_PIXEL_I(imIn, x, yy) * k[x - (int) xmin];
-                        IMAGING_PIXEL_I(imOut, xx, yy) = (int) ss * ww;
-                    }
-                    break;
-                case IMAGING_TYPE_FLOAT32:
-                    /* 32-bit float */
-                    for (yy = 0; yy < imOut->ysize; yy++) {
-                        ss = 0.0;
-                        for (x = (int) xmin; x < (int) xmax; x++)
-                            ss = ss + IMAGING_PIXEL_F(imIn, x, yy) * k[x - (int) xmin];
-                        IMAGING_PIXEL_F(imOut, xx, yy) = ss * ww;
-                    }
-                    break;
-                default:
-                    ImagingSectionLeave(&cookie);
-                    return (Imaging) ImagingError_ModeError();
+                switch (imIn->type) {
+                  case IMAGING_TYPE_UINT8:
+                      /* n-bit grayscale */
+                      for (yy = 0; yy < imOut->ysize; yy++) {
+                          for (b = 0; b < imIn->bands; b++) {
+                              if (imIn->bands == 2 && b)
+                                  b = 3;        /* hack to deal with LA images */
+                              ss = 0.0;
+                              for (x = (int) xmin; x < (int) xmax; x++)
+                                  ss = ss + (UINT8) imIn->image[yy][x * 4 + b] * k[x - (int) xmin];
+                              ss = ss * ww + 0.5;
+                              if (ss < 0.5)
+                                  imOut->image[yy][xx * 4 + b] = (UINT8) 0;
+                              else if (ss >= 255.0)
+                                  imOut->image[yy][xx * 4 + b] = (UINT8) 255;
+                              else
+                                  imOut->image[yy][xx * 4 + b] = (UINT8) ss;
+                          }
+                      }
+                      break;
+                  case IMAGING_TYPE_INT32:
+                      /* 32-bit integer */
+                      for (yy = 0; yy < imOut->ysize; yy++) {
+                          ss = 0.0;
+                          for (x = (int) xmin; x < (int) xmax; x++)
+                              ss = ss + IMAGING_PIXEL_I(imIn, x, yy) * k[x - (int) xmin];
+                          IMAGING_PIXEL_I(imOut, xx, yy) = (int) ss *ww;
+                      }
+                      break;
+                  case IMAGING_TYPE_FLOAT32:
+                      /* 32-bit float */
+                      for (yy = 0; yy < imOut->ysize; yy++) {
+                          ss = 0.0;
+                          for (x = (int) xmin; x < (int) xmax; x++)
+                              ss = ss + IMAGING_PIXEL_F(imIn, x, yy) * k[x - (int) xmin];
+                          IMAGING_PIXEL_F(imOut, xx, yy) = ss * ww;
+                      }
+                      break;
+                  default:
+                      ImagingSectionLeave(&cookie);
+                      return (Imaging) ImagingError_ModeError();
                 }
         }
     }
